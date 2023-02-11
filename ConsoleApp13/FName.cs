ba5b8b939b794bfdcfeb84649bdd8f2721e36d11
Vovan4ik <pologonik555@gmail.com>
2023-02-11 23:10:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/FName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class FName
+    {
+        public FName() { }
+        public FName(string SurName, string Name, string FathName)
+        {
+            string FullName = SurName + "." + Name +"."+ FathName;
+        }
+        public string SurName { get; set; }
+        public string Name { get; set; }
+        public string FathName { get; set; }
+
+        public override string ToString()
+        {
+            return "(" + SurName + " " + Name + " " + FathName + ")";
+        }
+        
+    }
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp13;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+string[] FathNames = { "Владимир", "Виталя", "Игор", "Альберт", "Мурад"};
+string[] SurNames = { "Владимир", "Виталя", "Игор", "Альберт", "Мурад" };
+string[] Names = { "Владимир", "Виталя", "Игор", "Альберт", "Мурад" };
+
+Random random = new Random();
+    FName[] FullName = new FName[5] { new FName(), new FName(), new FName(), new FName(), new FName() };
+    for (int i = 0; i < FullName.Length; i++)
+    {
+    FullName[i].Name= FathNames[random.Next(5)];
+    FullName[i].SurName = SurNames[random.Next(5)];
+    FullName[i].FathName = Names[random.Next(5)];
+    Console.WriteLine(FullName[i].SurName + " " + FullName[i].Name+ " " + FullName[i].FathName);
+    }
+Console.ReadKey();
+
+//random.Next(5);
+
+/* { "qq", "qq", "qq" },{"gg","ez","aga" } };*/
+
+
+
+
+
+//string[] FathNames = { "Владимир", "Виталя", "Игор", "Альберт", "Мудар", "Джиган", "Артур", "Даниил", "Максим", "Джон" };
+//В классе Program.cs создать массив из 5 экземпляров данного класса и вывести его на экран
